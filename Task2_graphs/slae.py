b4ba88a04211fc2d97d146ca4d6cc5dc6c93e31d
Mihail Romanyuk <mikro@HomePCM>
2024-02-19 11:57:01 +0300

    A bit repair testspeed

--- Task2_graphs/slae.py
+++ Task2_graphs/slae.py
@@ -1,14 +1,11 @@
 import matplotlib.pyplot as pyplot
 import numpy as np
-from scipy.optimize import curve_fit
-import matplotlib.ticker as ticker
 
-name = 'ass2.txt'
-ys1 = np.loadtxt('result1.txt')
-ys2 = np.loadtxt('result2.txt')
-ys3 = np.loadtxt('result3.txt')
-ys4 = np.loadtxt('result4.txt')
-xs = np.arange(1, 1505, 5)
+ys1 = np.loadtxt('build/result1.txt')
+ys2 = np.loadtxt('build/result2.txt')
+ys3 = np.loadtxt('build/result3.txt')
+ys4 = np.loadtxt('build/result4.txt')
+xs = np.arange(100, 2000, 10)
 
 fig, ax = pyplot.subplots(figsize=(12, 8), dpi=400)
 
@@ -17,13 +14,12 @@ ax.minorticks_on()
 ax.grid()
 ax.grid(which='minor', linestyle=':')
 
-ax.plot(xs, ys1, zorder=10, linewidth=2, label='Плотная матрица в обычном виде')
-ax.plot(xs, ys2, zorder=10, linewidth=2, label='Матрица с нулями в обычном виде')
-ax.plot(xs, ys3, zorder=10, linewidth=2, label='Плотная матрица CSR')
-ax.plot(xs, ys4, zorder=10, linewidth=2, label='Матрица с нулями CSR')
+ax.plot(xs, ys1, zorder=10, linewidth=2, label='Плотная матрица, хранимая в обычном виде')
+ax.plot(xs, ys2, zorder=10, linewidth=2, label='Плотная матрица, хранимая в виде CSR')
+ax.plot(xs, ys3, zorder=10, linewidth=2, label='Матрица с нулями, хранимая в обычном виде')
+ax.plot(xs, ys4, zorder=10, linewidth=2, label='Матрица с нулями, хранимая в виде CSR')
 ax.set_xlabel('Размер одной стороны квадратной матрицы', fontsize=14, fontname='Georgia')
 ax.set_ylabel('Время в микросекундах', fontsize=14, fontname='Georgia')
-ax.legend(loc='lower right')
+ax.legend(loc='upper left')
 pyplot.savefig("Graph 1.jpg", bbox_inches='tight')
 
-pyplot.show()
--- Task2_graphs/testspeed.cpp
+++ Task2_graphs/testspeed.cpp
@@ -15,15 +15,18 @@ private:
     steady_clock::time_point start;
     std::fstream& writer;
 };
+
 int main(){
     std::fstream out1("result1.txt", std::ios_base::app);
     std::fstream out2("result2.txt", std::ios_base::app);
     std::fstream out3("result3.txt", std::ios_base::app);
     std::fstream out4("result4.txt", std::ios_base::app);
-    for(auto k = 100u; k < 2000; k+=5){
+    //Прогоняем матрицы разных размеров
+    for(auto k = 100u; k < 2000u; k+=10){
         auto dense_array = std::vector<std::vector<double>>(k, std::vector<double>(k));
         auto csr_array = std::vector<std::vector<double>>(k, std::vector<double>(k));
         auto vec = std::vector<double>(k);
+        //Заполняем матрицу плотно
         for (auto i = 0u; i < k; i++){
             for(auto j = 0u; j < k; j++){
                 std::srand(std::time(nullptr)); // use current time as seed for random generator
@@ -31,23 +34,26 @@ int main(){
                 dense_array[i][j] = static_cast<double>(random_value);
             }
         }
+        //Заполняем вектор
         for (auto i = 0u; i < k; i++){
-            std::srand(std::time(nullptr)); // use current time as seed for random generator
+            std::srand(std::time(nullptr));
             int random_value = std::rand();
             vec[i] = static_cast<double>(random_value);
         }
+        //Заполняем матрицу с большим количсетвом нулей (по всем чётным i или j)
         for (auto i = 0u; i < k; i++){
             for(auto j = 0u; j < k; j++){
                 if(i % 2 == 0 || j % 2 == 0){
                     csr_array[i][j] = 0;
                 }
                 else{
-                    std::srand(std::time(nullptr)); // use current time as seed for random generator
+                    std::srand(std::time(nullptr));
                     int random_value = std::rand();
-                    dense_array[i][j] = static_cast<double>(random_value);
+                    csr_array[i][j] = static_cast<double>(random_value);
                 }
             }
         }
+        
         auto matrix1 = cmake_example::Matrix(dense_array);
         auto matrix2 = cmake_example::Matrix_CSR(dense_array);
         auto matrix3 = cmake_example::Matrix(csr_array);
