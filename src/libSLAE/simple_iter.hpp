17204a8f674fccc5f465e59754196c0f34450e68
Mikhail Romanyuk <romaniuk.mia@phystech.edu>
2024-03-08 23:18:24 +0300

    Add Chebyshev (no tests speed yet)

--- src/libSLAE/simple_iter.hpp
+++ src/libSLAE/simple_iter.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include "dense_CSR.hpp"
+#include <cmath>
+#define _USE_MATH_DEFINES
 
 namespace simple_iter{
     using vector = std::vector<double>;
@@ -12,4 +14,7 @@ namespace simple_iter{
 
     vector simple_iter(const dense_CSR::Matrix_CSR& A, const vector& x_0, const vector& b, double tau,
     double target_discrepancy, unsigned frequency_checking, unsigned max_iteration);
+
+    //Возвращает 2^n корней на отрезке [lambda_min, lambda_max] в оптимальном порядке
+    vector find_Chebyshev_roots(unsigned n, double lambda_min, double lambda_max);
 }
---
+++ tests/test_Chebyshev.cpp
@@ -0,0 +1,30 @@
+#include <gtest/gtest.h>
+#include <libSLAE/simple_iter.hpp>
+
+// TEST(Matrix_test, first_matrix) {
+//     std::vector<std::vector<double>> arr = 
+//     std::vector<std::vector<double>>({{0, 2, 0}, {2, 0, 4}, {1, 2, 0}});
+//     auto mat = dense_CSR::Matrix(arr);
+//     auto mat_csr = dense_CSR::Matrix_CSR(arr);
+//     std::vector<double> v = std::vector<double>({1, 5, 3});
+//     auto out1 = mat * v;
+//     auto out2 = mat_csr * v;
+//     ASSERT_NEAR(10.0, out1[0], 0.0001);
+//     ASSERT_NEAR(14.0, out1[1], 0.0001);
+//     ASSERT_NEAR(11.0, out1[2], 0.0001);
+//     ASSERT_NEAR(10.0, out2[0], 0.0001);
+//     ASSERT_NEAR(14.0, out2[1], 0.0001);
+//     ASSERT_NEAR(11.0, out2[2], 0.0001);
+// }
+
+// int main(int argc, char **argv) {
+int main(){
+    auto vector = simple_iter::find_Chebyshev_roots(3, 1, 2);
+    for(auto i = 0u; i< vector.size(); i++){
+        std::cout << vector[i] << " ";
+    }
+    std::cout << std::endl;
+    // testing::InitGoogleTest(&argc, argv);
+    // return RUN_ALL_TESTS();
+}
+
